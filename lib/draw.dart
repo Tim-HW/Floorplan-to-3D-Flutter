4aaf9b57b5b161dfb0a2b7f66a8dd82dcee3fbdd
Tim-Desktop <timothee.freville@mil.be*>
2023-03-09 16:05:03 +0100

    Drawer seems to work

--- lib/draw.dart
+++ lib/draw.dart
@@ -1,122 +1,144 @@
-
-import 'dart:ffi';
 import 'dart:ui' as ui;
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'dart:math' as math;
 
+class FacePainter extends CustomPainter {
+  FacePainter(this.image, this.positionStart, this.positionEnd, this.ListDoors,
+      this.ListWindow, this.IsADoor);
 
-
-
-
-class FacePainter extends CustomPainter{
-  
-  FacePainter(this.image,this.positionStart,this.positionEnd,this.ListDoors,this.ListWindow,this.IsADoor);
+  // To know if the door/windows is selected
   final bool IsADoor;
+  // List of Doors
   final List<Rect> ListDoors;
+  // List of Windows
   final List<Rect> ListWindow;
+  // Background Image
   final ui.Image image;
+  // Current startposition
   final Offset positionStart;
+  // Current Endposition
   final Offset positionEnd;
 
-
-  late double x = positionEnd.dx - positionStart.dx;
-  late double y = positionEnd.dy - positionStart.dy;
-
+  // Color for Windows
   Color colorWindows = ui.Color.fromARGB(255, 27, 0, 179);
-  Color colorDoors   = ui.Color.fromARGB(255, 0, 179, 95);
+  // Color for Doors
+  Color colorDoors = ui.Color.fromARGB(255, 0, 179, 95);
 
-
-  void paint(Canvas canvas, ui.Size size){
+  // Main function to print on the canvas
+  void paint(Canvas canvas, ui.Size size) {
+    // Upload image on the background
     canvas.drawImage(image, Offset.zero, Paint());
-    
-    for(var i = 0 ; i < ListDoors.length; i++)
-    {
-      canvas.drawRect(ListDoors[i], Paint()..color=colorDoors);
+    // Render the door list
+    for (var i = 0; i < ListDoors.length; i++) {
+      canvas.drawRect(ListDoors[i], Paint()..color = colorDoors);
+    }
+    // Render the Window list
+    for (var j = 0; j < ListWindow.length; j++) {
+      canvas.drawRect(ListWindow[j], Paint()..color = colorWindows);
     }
 
-    for(var j = 0 ; j < ListWindow.length; j++)
-    {
-      canvas.drawRect(ListWindow[j], Paint()..color=colorWindows);
+    // If the current object is a door render it
+    if (IsADoor) {
+      double x = positionEnd.dx - positionStart.dx;
+      double y = positionEnd.dy - positionStart.dy;
+      canvas.drawRect(
+          positionStart & ui.Size(x, y), Paint()..color = colorDoors);
+    } else {
+      // If the current object is a window render it
+      double x = positionEnd.dx - positionStart.dx;
+      double y = positionEnd.dy - positionStart.dy;
+      canvas.drawRect(
+          positionStart & ui.Size(x, y), Paint()..color = colorWindows);
     }
-
-    if(IsADoor){
-      canvas.drawRect(positionStart & ui.Size(x,y), Paint()..color=colorDoors);
-    }
-    else
-    {
-    canvas.drawRect(positionStart & ui.Size(x,y), Paint()..color=colorWindows);
-    }
-   
-    
-
-
   }
-  @override
-  bool shouldRepaint(FacePainter oldDelegate){
 
-    if(image != oldDelegate.image || positionStart != oldDelegate.positionStart || positionEnd != oldDelegate.positionEnd || ListDoors != oldDelegate.ListDoors || ListWindow != oldDelegate.ListWindow)
-    {return true;}
-    else
-    {return false;}
+  @override
+  // This function triggers the main function everytime an element change
+  bool shouldRepaint(FacePainter oldDelegate) {
+    if (image != oldDelegate.image ||
+        positionStart != oldDelegate.positionStart ||
+        positionEnd != oldDelegate.positionEnd ||
+        ListDoors != oldDelegate.ListDoors ||
+        ListWindow != oldDelegate.ListWindow ||
+        IsADoor != oldDelegate.IsADoor) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
 class DrawImage extends StatefulWidget {
-
-
   @override
   _DrawImageState createState() => _DrawImageState();
 }
 
 class _DrawImageState extends State<DrawImage> {
-
+  /*
+################################################################
+                          VARIABLES
+################################################################
+*/
+  // Backgronud image
   late ui.Image _Background;
-
+  // Which item is selected
   bool IsDoorsAndWindows = false;
-
+  // Current start position
   Offset _PositionStart = Offset(0, 0);
+  // Current end position
   Offset _PositionEnd = Offset(0, 0);
+  // List of door
+  List<Rect> Doors = List.empty(growable: true);
+  // List of window
+  List<Rect> Windows = List.empty(growable: true);
 
+  /*
+################################################################
+                          FUNCTIONS
+################################################################
+*/
 
-  final List<Rect> Doors   = List.empty(growable: true);
-  final List<Rect> Windows = List.empty(growable: true);
-  List<int> test     = [];
-
-  
-
-  
   @override
+  // Init function to load the background
   void initState() {
-  _asyncInit();
-}
+    _asyncInit();
+  }
 
-
-  void ChangeObject(bool value)
-  {
+  // Function to change the door/window selection
+  void ChangeObject(bool value) {
     IsDoorsAndWindows = value;
   }
 
-  Future<void> _asyncInit() async {
-    final image = await loadImage('./assets/VR.png');
+  void ErasePrevious() {
     setState(() {
+      if (IsDoorsAndWindows) {
+        Doors.removeLast();
+      } else {
+        Windows.removeLast();
+      }
+    });
+  }
+
+  void Erasedall() {
+    setState(() {
+      // Update the variable
+      Doors = List.empty(growable: true);
+      Windows = List.empty(growable: true);
+    });
+  }
+
+  // Function to load the Background
+  Future<void> _asyncInit() async {
+    // Load the image
+    final image = await loadImage('assets/VR.png');
+    setState(() {
+      // Update the variable
       _Background = image;
     });
   }
 
+  // Load image function
   Future<ui.Image> loadImage(imageString) async {
     ByteData bd = await rootBundle.load(imageString);
     // ByteData bd = await rootBundle.load("graphics/bar-1920×1080.jpg");
@@ -127,132 +149,130 @@ class _DrawImageState extends State<DrawImage> {
     // setState(() => imageStateVarible = image);
   }
 
-
-  void _getTapPosition(TapDownDetails details) async 
-  {
+  // Function to get the position tapped
+  void _getTapPosition(TapDownDetails details) async {
+    // init variable
     final tapPosition = details.globalPosition;
     setState(() {
+      // Update variable
+      _PositionStart = Offset(tapPosition.dx, tapPosition.dy - 56);
 
-      _PositionStart = Offset(tapPosition.dx,tapPosition.dy-56);
-      
-      
       print(_PositionStart);
-    
     });
   }
 
-
-  void _getStartPosition(DragStartDetails details) async 
-  {
+  void _getStartPosition(DragStartDetails details) async {
     final tapPosition = details.globalPosition;
     setState(() {
+      _PositionStart = Offset(tapPosition.dx, tapPosition.dy - 56);
 
-      _PositionStart = Offset(tapPosition.dx,tapPosition.dy-56);
-      
-      
-      print('Start : '+_PositionStart.toString());
-    
+      print('Start : ' + _PositionStart.toString());
     });
   }
 
-  void _getEndPosition(DragUpdateDetails details) async 
-  {
+  void _getEndPosition(DragUpdateDetails details) async {
     final tapPosition = details.globalPosition;
     setState(() {
+      _PositionEnd = Offset(tapPosition.dx, tapPosition.dy - 56);
 
-      _PositionEnd = Offset(tapPosition.dx,tapPosition.dy-56);
-
-      print('End : '+tapPosition.toString());
-    
+      print('End : ' + tapPosition.toString());
     });
   }
 
-  void _getEnd(DragEndDetails details) async 
-  {
+  void _getEnd(DragEndDetails details) async {
     final value = details.velocity.toString();
     setState(() {
+      if (value != null) {
+        print('Value : ' + value);
+        if (IsDoorsAndWindows) {
+          double X2 = _PositionEnd.dx - _PositionStart.dx;
+          double Y2 = _PositionEnd.dy - _PositionStart.dy;
 
-      if(value != null)
-      {
-        print('Value : '+value);
-        if(IsDoorsAndWindows)
-        {
-
-          double X2   = _PositionEnd.dx - _PositionStart.dx;
-          double Y2   = _PositionEnd.dy - _PositionStart.dy;
-
-          Rect myRect = _PositionStart & ui.Size(X2,Y2);
+          Rect myRect = _PositionStart & ui.Size(X2, Y2);
           Doors.add(myRect);
+
           print(Doors);
+        } else {
+          double X2 = _PositionEnd.dx - _PositionStart.dx;
+          double Y2 = _PositionEnd.dy - _PositionStart.dy;
 
-        }else{
-
-          double X2   = _PositionEnd.dx - _PositionStart.dx;
-          double Y2   = _PositionEnd.dy - _PositionStart.dy;
-
-          Rect myRect = _PositionStart & ui.Size(X2,Y2);
+          Rect myRect = _PositionStart & ui.Size(X2, Y2);
           Windows.add(myRect);
           //print(Doors);
 
         }
-        
+        _PositionStart = Offset(0, 0);
+        _PositionEnd = Offset(0, 0);
       }
     });
-
   }
 
-
+/*
+################################################################
+                          BUILD WIDGET
+################################################################
+*/
 
   Widget build(BuildContext context) {
-    return 
-    Scaffold(
-      body:
-      GestureDetector(
-      //onTapDown: (details) => _getTapPosition(details),
-
-      onPanStart: (details) => _getStartPosition(details),
-      onPanUpdate: (details) => _getEndPosition(details),
-      onPanEnd: (details) => _getEnd(details),
-      
-        child: 
-          FittedBox(
-          child: 
-          SizedBox(
-            width:  _Background.width.toDouble(),
+    return Scaffold(
+      body: GestureDetector(
+        // Function to update start position of the drag
+        onPanStart: (details) => _getStartPosition(details),
+        // Function to update the current position of the drag
+        onPanUpdate: (details) => _getEndPosition(details),
+        // Function to trigger the end of the drag event
+        onPanEnd: (details) => _getEnd(details),
+        // Actual canvas rendering
+        child: FittedBox(
+          child: SizedBox(
+            // Canvas takes the width of the image
+            width: _Background.width.toDouble(),
+            // Canvas takes the height of the image
             height: _Background.height.toDouble(),
-            child: 
-              CustomPaint(
-                painter: FacePainter(_Background,_PositionStart,_PositionEnd,Doors,Windows,IsDoorsAndWindows),
-            ), 
+            // Render the canvas
+            child: CustomPaint(
+              painter: FacePainter(_Background, _PositionStart, _PositionEnd,
+                  Doors, Windows, IsDoorsAndWindows),
+            ),
           ),
         ),
       ),
-      floatingActionButton: SpeedDial(
-        icon: Icons.add,
-        backgroundColor: Colors.red,
-
-        children: [
-          SpeedDialChild(
-            child: const Icon(Icons.door_back_door,color: Colors.white),
-            label: 'Door',
-            backgroundColor: Colors.red,
-            onTap: () {
-              ChangeObject(true);
-            },
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.window,color: Colors.white),
-            label: 'Window',
-            backgroundColor: Colors.red,
-            onTap: () {
-              ChangeObject(false);
-            },
-          ),
-        ]
-      ),
+      // Add floating button to switch between doors and windows
+      floatingActionButton:
+          SpeedDial(icon: Icons.add, backgroundColor: Colors.red, children: [
+        SpeedDialChild(
+          child: const Icon(Icons.door_back_door, color: Colors.white),
+          label: 'Door',
+          backgroundColor: Colors.red,
+          onTap: () {
+            ChangeObject(true);
+          },
+        ),
+        SpeedDialChild(
+          child: const Icon(Icons.window, color: Colors.white),
+          label: 'Window',
+          backgroundColor: Colors.red,
+          onTap: () {
+            ChangeObject(false);
+          },
+        ),
+        SpeedDialChild(
+          child: const Icon(Icons.replay, color: Colors.white),
+          label: 'Erase previous object',
+          backgroundColor: Colors.red,
+          onTap: () {
+            ErasePrevious();
+          },
+        ),
+        SpeedDialChild(
+          child: const Icon(Icons.do_not_disturb, color: Colors.white),
+          label: 'Erase everything',
+          backgroundColor: Colors.red,
+          onTap: () {
+            Erasedall();
+          },
+        ),
+      ]),
     );
   }
 }
-
-
-
